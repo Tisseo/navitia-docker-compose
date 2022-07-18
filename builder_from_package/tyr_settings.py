a9551d2be7628d977c6fc57db6955f0824f86487
Abderrahim AZIME <azime_1@yahoo.fr>
2022-07-18 09:59:41 +0200

    Env variables added for tyr

--- builder_from_package/tyr_settings.py
+++ builder_from_package/tyr_settings.py
@@ -1,11 +1,15 @@
-CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
-KRAKEN_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+import os
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://navitia:navitia@database/jormungandr'
+CELERY_BROKER_URL = os.getenv('TYR_CELERY_BROKER_URL', 'amqp://guest:guest@rabbitmq:5672//')
+KRAKEN_BROKER_URL = os.getenv('TYR_KRAKEN_BROKER_URL', 'amqp://guest:guest@rabbitmq:5672//')
+
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    'TYR_SQLALCHEMY_DATABASE_URI', 'postgresql://navitia:navitia@database/jormungandr'
+)
 
 # If the presence of the cities db is not needed, un-comment the following line
 # CITIES_DATABASE_URI = None
 
-REDIS_HOST = 'redis'
+REDIS_HOST = os.getenv('TYR_REDIS_HOST', 'redis')
 
-INSTANCES_DIR = '/etc/tyr.d'
+INSTANCES_DIR = os.getenv('TYR_INSTANCES_DIR', '/etc/tyr.d')
