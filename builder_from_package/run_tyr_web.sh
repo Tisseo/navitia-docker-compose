56b583c19753f57b23bccb0b0aa9fd8b2cef2e15
patrick.qian <patrick.qian@kisio.com>
2022-07-29 11:16:18 +0200

    move jormungandr db migration in to tyr-web

--- builder_from_package/run_tyr_web.sh
+++ builder_from_package/run_tyr_web.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-echo ${TYR_SQLALCHEMY_DATABASE_URI}
 # we need to wait for the database to be ready
-while ! pg_isready --host=${TYR_SQLALCHEMY_DATABASE_URI}; do
+while ! pg_isready --host=${TYR_DATABASE_HOST}; do
     echo "waiting for postgres to be ready"
     sleep 1;
 done
 
-# export PYTHONPATH=.:../navitiacommon
-
 #db migration
 python /usr/bin/manage_tyr.py db upgrade
+echo "MIGRATION IS FINISHED"
 
 uwsgi --mount /=tyr:app --http 0.0.0.0:80
